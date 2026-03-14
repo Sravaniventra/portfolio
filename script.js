5f31255d18f59b1c8c3bde9394695356a1bcdc05
Sravani Ventra <sravaniventra@gmail.com>
2026-03-14 19:33:23 +0530

    fixed js file

--- script.js
+++ script.js
@@ -201,6 +201,15 @@ window.closeModal = function(){
 document.getElementById('modal-container').innerHTML = '';
 }
 
+
+
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+
+menuToggle.addEventListener("click", () => {
+navMenu.classList.toggle("active");
+}); 
+
 const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener("click", () => {
